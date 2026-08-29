a221903c83507815ccf95d83d865b7b00555b9bd
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-08-29 18:34:58 +0530

    Society Management API

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,11 +14,4 @@ app.use(express.json());
 // api will go to index.js in routes
 app.use('/api', require('./routes/index.route'));
 
-app.listen(process.env.PORT, err => {
-    if (err) {
-        console.log("Server is not started...");
-        return;
-    }
-
-    console.log("Server is started...");
-});
+module.exports = app;
