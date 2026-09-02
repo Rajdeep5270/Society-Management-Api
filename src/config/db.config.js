320432994cc440f32198970523a90838e7bfbedc
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-09-02 23:27:47 +0530

    Society-Management-API

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -17,7 +17,7 @@ const connectDB = async () => {
 
     // Connection already in progress
     if (!cached.promise) {
-        cached.promise = mongoose.connect(process.env.MONGO_URI, {
+        cached.promise = mongoose.connect(process.env.MONGOBBURI, {
             maxPoolSize: 10,
             serverSelectionTimeoutMS: 10000,
             bufferCommands: false,
