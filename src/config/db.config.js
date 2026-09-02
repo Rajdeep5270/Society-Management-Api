8d7dd28cefd4d370c58b01baec5e4ddf242d9e85
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-09-02 23:41:07 +0530

    Society Management API

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODBURI).then(() => {
-    console.log("Database is connected...");
-}).catch(err => {
-    console.log("Database is not connected...", err);
-});    
+const connectDatabase = async () => {
+    if (!process.env.MONGODBURI) {
+        console.log('MONGODBURI environment variable is missing');
+        return;
+    }
+
+    await mongoose.connect(process.env.MONGODBURI);
+    console.log('Database is connected...');
+};
+
+module.exports = connectDatabase;
