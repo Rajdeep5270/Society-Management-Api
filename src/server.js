4cc3af24e3be708ae8bf3b4414f41be55cca9725
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-09-02 23:18:09 +0530

    Society Management API

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-require('./config/db.config');
+const connectDB = require('./config/db.config');
 
 const cors = require('cors');
 
@@ -10,6 +10,15 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(async (req, res, next) => {
+    try {
+        await connectDB();
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
 // api will go to index.js in routes
 app.use('/api', require('./routes/index.route'));
