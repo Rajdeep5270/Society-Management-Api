0c3201fbb4c8f219ebe8e530bae1c65a7e23c89b
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-09-20 22:21:02 +0530

    Society Management API

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 require('./config/db.config');
 
@@ -8,48 +7,13 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-const allowedOrigins = [
-    'http://localhost:5173',
-    // 'https://your-frontend-domain.vercel.app'
-];
-
-app.use(cors({
-    origin: (origin, callback) => {
-
-        // Postman ya server-to-server requests ke liye
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }
-
-        return callback(
-            new Error('Not allowed by CORS')
-        );
-    },
-    credentials: true,
-    methods: [
-        'GET',
-        'POST',
-        'PUT',
-        'PATCH',
-        'DELETE',
-        'OPTIONS'
-    ],
-    allowedHeaders: [
-        'Content-Type',
-        'Authorization'
-    ]
-}));
-
+app.use(cors());
 app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// API routes
+// api will go to index.js in routes
 app.use('/api', require('./routes/index.route'));
 
 // app.listen(process.env.PORT, (err) => {
@@ -59,6 +23,6 @@ app.use('/api', require('./routes/index.route'));
 //     }
 
 //     console.log("Server is started");
-// });
+// })
 
 module.exports = app;
